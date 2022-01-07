0d5fedc87cf6c17948413c299d648c978f82573f
BuiTheBoi <uNUKEcorn15@gmail.com>
2022-01-07 02:42:09 -0800

    Implemented scrolling animations using JavaScript :alien: :star2:
    May not be the most efficient JavaScript code, but it is subject to optimize over time as I improve on JavaScript.

---
+++ Scripts/nav.js
@@ -0,0 +1,30 @@
+// Clicking navbar buttons to scroll to specific sections of website
+$(".nav-item").on("click", function (e) {
+    if (this.hash !== "") {
+        e.preventDefault(); // Preventing the default behavior when clicking buttons
+        const hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top
+        }, 800);    // scrolling from the top taking 800 ms
+    }
+});
+
+// Clicking link saying "Contact Me Below!" in About Me section
+$(".scroll-to-contacts").on("click", function (e) {
+    console.log(this.hash);
+    if (this.hash !== "") {
+        e.preventDefault();
+        const hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    }
+});
+
+// const scrollToBott = document.querySelector("#btnScrollToBottom");
+
+// scrollToBott.addEventListener("click", function () {
+//     console
+// });
