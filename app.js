c643fb51b07874a39bf9f7a1eb2d56d6e11c8b61
JustinBui <jbui3493@gmail.com>
2023-03-14 13:16:21 -0700

    Fixed card title colors and integraded Typed.js

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Gobble d gook");
+
+var text = new Typed(".auto-type", {
+    strings: ["Welcome to my portfolio!", 
+            "Software Engineering enthusiast",
+            "Data Science/Machine Learning enthusiast",
+            "Python lover",
+            "Brogrammer",
+            "Gym rat"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: true
+});
